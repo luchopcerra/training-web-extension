e195480df76395a0bd93121d485af6ffa7bb1512
Luciano Pérez Cerra <luchopcerra@gmail.com>
2018-08-28 22:31:20 -0300

    Prettier formatter (no side effect)

--- content_scripts/index.js
+++ content_scripts/index.js
@@ -1,4 +1,3 @@
-
-let words = new WordsFromDom("H1", (word)=>word.length>5);
+let words = new WordsFromDom("H1", word => word.length > 5);
 // messages passing with background script to display results
-browser.runtime.sendMessage({"words": words.toLookUp()});
+browser.runtime.sendMessage({ words: words.toLookUp() });
--- shared/wordsFromDom.js
+++ shared/wordsFromDom.js
@@ -1,28 +1,28 @@
 class WordsFromDom {
-    constructor(fromTag = 'H1', condition = ()=>true){
-        this.fromTag = fromTag;
-        this.condition = condition;
-        this.words =    Array.from(document.getElementsByTagName(fromTag))
-                            .map(h1=>h1.textContent.split(" ").filter(condition))
-                            // acá voy a usar un reduce and concat para implementar un flatten
-                            .reduce((acc, val) => acc.concat(val), [])
-    }
+  constructor(fromTag = "H1", condition = () => true) {
+    this.fromTag = fromTag;
+    this.condition = condition;
+    this.words = Array.from(document.getElementsByTagName(fromTag))
+      .map(h1 => h1.textContent.split(" ").filter(condition))
+      // acá voy a usar un reduce and concat para implementar un flatten
+      .reduce((acc, val) => acc.concat(val), []);
+  }
 
-    set condition(condition = ()=>true){
-        this._condition = condition
-    }
-    get condition(){
-        return this._condition
-    }
+  set condition(condition = () => true) {
+    this._condition = condition;
+  }
+  get condition() {
+    return this._condition;
+  }
 
-    set fromTag(fromTag = 'H1'){
-        this._fromTag = fromTag
-    }
-    get fromTag(){
-        return this._fromTag
-    }
+  set fromTag(fromTag = "H1") {
+    this._fromTag = fromTag;
+  }
+  get fromTag() {
+    return this._fromTag;
+  }
 
-    toLookUp (){
-        return this.words
-    }
+  toLookUp() {
+    return this.words;
+  }
 }
